a87fd22e93dd0c0e8d20c9f2029103ea28fac681
salim kapasi <sgkapasi@gmail.com>
2021-06-01 11:06:58 -0400

    dom sanitizer test

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AuthService } from '../auth.service';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthService} from '../auth.service';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
@@ -9,10 +10,13 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  htmlParsed: any;
   form;
+
   constructor(private fb: FormBuilder,
-    private myRoute: Router,
-    private auth: AuthService) {
+              private myRoute: Router,
+              private auth: AuthService,
+              private domSanitizer: DomSanitizer) {
     this.form = fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
@@ -20,13 +24,23 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.htmlParsed = this.domSanitizer.bypassSecurityTrustHtml(`
+<button type="button" (click)="alertz()">alert</button>
+<a href="https://www.google.com">me</a>
+`);
+  }
+
+  alertz() {
+    console.log('sasa');
   }
 
   login() {
     if (this.form.valid) {
-      this.auth.sendToken(this.form.value.email)
-      this.myRoute.navigate(["home"]);
+      this.auth.sendToken(this.form.value.email);
+      this.myRoute.navigate(['home']);
     }
   }
+
+
 
 }
